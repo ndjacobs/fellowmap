67e23cbc221524485b8e9fb06009829a2f0de453
ndjacobs <n.d.jacobs@gmail.com>
2020-05-15 10:35:56 -0500

    Initial Commit

---
+++ Fellowmap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fellowmap
+//
+//  Created by Nathan Jacobs on 5/15/20.
+//  Copyright © 2020 Nathan Jacobs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
